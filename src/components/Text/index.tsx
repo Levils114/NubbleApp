89b8def9cf8b4212398d864aabdaccbd5479bf98
Levils114 <levils114@gmail.com>
2023-06-14 23:17:29 -0300

    feat: create Text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  TextStyle,
+} from 'react-native';
+
+interface TextProps extends RNTextProps {
+  preset?: TypographyVariants;
+}
+
+type TypographyVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+const textStylesMap: Record<TypographyVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+export function Text({
+  children,
+  style,
+  preset = 'paragraphMedium',
+  ...props
+}: TextProps) {
+  const styleMapped = textStylesMap[preset];
+
+  return (
+    <RNText style={[styleMapped, style]} {...props}>
+      {children}
+    </RNText>
+  );
+}
